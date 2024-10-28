f60577b7177fed110ea80fbade5e4d3166b5fee0
Adithyan A <deepaadithyan56@gmail.com>
2024-10-28 22:53:32 +0530

    REFORMAT: Format code

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,16 +1,21 @@
 import { defineQuery } from "groq";
 
-export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+export const STARTUPS_QUERY = defineQuery(`
+*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, _createdAt, title, views, description, category, image,
   author -> { _id, name, image, bio },
-}`);
+}
+`);
 
-export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+export const STARTUP_BY_ID_QUERY = defineQuery(`
+*[_type == "startup" && _id == $id][0]{
   _id, _createdAt, title, views, description, category, image, pitch,
   author -> { _id, name, image, bio, username },
-}`);
+}
+`);
 
-export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+*[_type == "startup" && _id == $id][0]{
   _id, views
-}`);
+}
+`);
