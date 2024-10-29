b1be68c1bb02430d094e61ed17e012a82c3936be
Adithyan A <deepaadithyan56@gmail.com>
2024-10-30 01:21:40 +0530

    Add utility function to get Initials of a name

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,10 @@ export function formatDate(date: string) {
     year: "numeric",
   });
 }
+
+export function getInitials(name: string | null | undefined) {
+  if (!name) return "";
+  const words = name.trim().split(" ");
+  if (words.length === 1) return words[0].charAt(0).toUpperCase();
+  return (words[0].charAt(0) + words[1].charAt(0)).toUpperCase();
+}
