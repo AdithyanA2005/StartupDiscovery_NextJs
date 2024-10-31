c22202c538897532aec4485977e5fc51ef8db5a0
Adithyan A <deepaadithyan56@gmail.com>
2024-10-31 17:49:13 +0530

    Create new scheme for "playlist" in sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { author } from "@/sanity/schemaTypes/author";
+import { playlist } from "@/sanity/schemaTypes/playlist";
 import { startup } from "@/sanity/schemaTypes/startup";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author, startup],
+  types: [author, startup, playlist],
 };
